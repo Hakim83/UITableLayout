ba1826b751f8ca6b0ba919caa43fad61f4e3a8d3
Mohammed Abdalhakam <mohammed.hakam@gmail.com>
2019-02-21 10:40:07 +0200

    Initial commit

---
+++ app/src/main/java/course/examples/ui/tablelayout/TableLayoutActivity.java
@@ -0,0 +1,13 @@
+package course.examples.ui.tablelayout;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class TableLayoutActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
